b18e7565a68de66e79325fbe92cd031f9d0f8d2b
Samarkin danya0 Danil <samarkin.danil@yandex.ru>
2022-07-24 19:46:26 +0300

    logo ready

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <h1>Hello</h1>
+  <div class="container mx-auto">
+    <AppLogo />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import AppLogo from '@/components/AppLogo.vue'
 
 export default defineComponent({
+  components: { AppLogo },
   data: () => {
     return {
       counter: 1
---
+++ src/components/AppLogo.vue
@@ -0,0 +1,16 @@
+<template>
+  <a class="block max-w-[400px] flex direction flex-col items-center" href="/">
+    <span class="text-9xl font-bold text-green-700 font-[Bitter]">Talkiee</span>
+    <div class="text-lg">Все фильмы и сериалы в одном месте</div>
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
+</style>
