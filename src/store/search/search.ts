4b39ac6cac86d73d1a7eb235d86ce77b9a7e1726
Samarkin danya0 Danil <samarkin.danil@yandex.ru>
2022-08-15 19:39:47 +0300

    Search mutations

--- src/store/search/search.ts
+++ src/store/search/search.ts
@@ -1,11 +1,20 @@
-import { Module } from 'vuex'
+import { Module, MutationTree } from 'vuex'
 import { SearchState } from '@/store/search/types'
 import { RootState } from '@/store/types'
+import * as SEARCH from './constants'
+import { Ref } from 'vue'
 
 const state: SearchState = {
   filmsGreedRef: null
 }
 
+const mutations: MutationTree<SearchState> = {
+  [SEARCH.SEARCH_SET_GRID_REF]: (state, ref: Ref) => {
+    state.filmsGreedRef = ref
+  }
+}
+
 export const search: Module<SearchState, RootState> = {
-  state
+  state,
+  mutations
 }
