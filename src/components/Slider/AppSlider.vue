7651ea81bd0370242cb1bbf8b33e6a32d2abaf90
Samarkin danya0 Danil <samarkin.danil@yandex.ru>
2022-08-15 23:54:01 +0300

    Добавил стрелки для слайдера

--- src/components/Slider/AppSlider.vue
+++ src/components/Slider/AppSlider.vue
@@ -15,8 +15,16 @@
       />
     </div>
     <div class="h-10 flex items-center">
-      <button class="mr-2" @click="prevSlide">Назад</button>
-      <button @click="nextSlide">Вперед</button>
+      <button
+        :disabled="currentSlide === 0"
+        class="disabled:cursor-not-allowed h-full arrow arrow-prev mr-3"
+        @click="prevSlide"
+      ></button>
+      <button
+        :disabled="currentSlide === testColors.length - 3"
+        class="disabled:cursor-not-allowed h-full arrow arrow-next"
+        @click="nextSlide"
+      ></button>
     </div>
   </div>
 </template>
@@ -57,3 +65,67 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+.arrow {
+  position: relative;
+  height: 10px;
+  width: 40px;
+}
+
+.arrow:before {
+  transition: 0.3s ease-in-out;
+  content: '';
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+
+  height: 4px;
+  background: #8c8c8c;
+}
+
+.arrow:first-child:before {
+  width: 30px;
+  left: 2px;
+}
+
+.arrow:first-child:hover:before {
+  width: 50px;
+}
+.arrow:first-child:hover + .arrow:before {
+  width: 30px;
+}
+
+.arrow:last-child:before {
+  opacity: 1;
+  background: #15803d;
+  right: 3px;
+  width: 50px;
+}
+
+.arrow:first-child::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 0;
+  height: 0;
+  border-top: 8px solid transparent;
+  border-bottom: 8px solid transparent;
+  border-right: 12px solid #8c8c8c;
+}
+
+.arrow:last-child:after {
+  content: '';
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 0;
+  height: 0;
+  border-top: 10px solid transparent;
+  border-bottom: 10px solid transparent;
+  border-left: 15px solid #15803d;
+}
+</style>
