dc0eed8d38472a485c2c9bcac348c180e3e73038
Samarkin danya0 Danil <samarkin.danil@yandex.ru>
2023-05-09 10:59:51 +0300

    feat(Container): на страницу favorite

--- src/pages/FavoritePage.vue
+++ src/pages/FavoritePage.vue
@@ -1,28 +1,28 @@
 <template>
-  <FilmsGrid just-grid :films-array="favoriteFilms">
-    <template #not-find-text>
-      <p class="text-xl flex items-center">
-        <span>В избранном пусто</span>
-        <span class="text-8xl">😱</span>
-      </p>
-    </template>
-  </FilmsGrid>
+  <AppContainer>
+    <FilmsGrid just-grid :films-array="favoriteFilms">
+      <template #not-find-text>
+        <p class="text-xl flex items-center">
+          <span>В избранном пусто</span>
+          <span class="text-8xl">😱</span>
+        </p>
+      </template>
+    </FilmsGrid>
+  </AppContainer>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import FilmsGrid from '@/components/FilmsGrid'
 import { mapState } from 'vuex'
+import AppContainer from '@/components/AppContainer.vue'
 
 export default defineComponent({
-  components: { FilmsGrid },
+  components: { AppContainer, FilmsGrid },
   computed: {
     ...mapState({
       favoriteFilms: (state) => state.favorite.favorite
     })
-  },
-  mounted() {
-    console.log('this.favoriteFilms -->', this.favoriteFilms)
   }
 })
 </script>
